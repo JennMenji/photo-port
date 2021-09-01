dd38399784fb400f85d6c6649956cf06c5d1f74b
Menji <jennifer.menjivar18@gmail.com>
2021-09-01 19:53:27 -0400

    about tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+
+afterEach(cleanup);
+
+describe("About component", () => {
+  // First Test: the baseline to verify that the component is rendering
+
+  it("renders", () => {
+    render(<About />);
+  });
+
+  // Second Test: to compare snapshot versions of the DOM node structure
+  it("matches snapsot DOM node structure", () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
