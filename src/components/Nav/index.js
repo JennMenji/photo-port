75620788c3c65aa0c2eda2f636215d190b8dfb49
Menji <jennifer.menjivar18@gmail.com>
2021-09-07 18:36:57 -0400

    conditionally rerendering pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,40 +1,19 @@
-import React from "react";
-
-const categories = [
-  {
-    name: "Commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  {
-    name: "Portraits",
-    descroption: "Portraits of people in my life",
-  },
-  {
-    name: "Food",
-    description: "Delicious delicacies",
-  },
-  {
-    name: "Landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  },
-];
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 // function categorySelected(name) {
 //   console.log(`${name} clicked`);
 // }
 
-function Nav() {
-  // function categorySelected() {
-  //   console.log("hello");
-  // }
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
 
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
@@ -55,9 +34,14 @@ function Nav() {
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActive"
+              }`}
+              key={category.name}
+            >
+              <span onClick={() => setCurrentCategory(category)}>
+                {capitalizeFirstLetter(category.name)}
               </span>
             </li>
           ))}
